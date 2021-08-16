90ca4af2655447dab2ccd2a8fba9da6bc6ca6fca
Sergio Larrion <sergio.larrion-vidorreta@capgemini.com>
2021-08-16 19:33:13 +0200

    Definimos los atributos de la clase comment

--- src/main/java/com/pizzeria/domain/commentdomain/Comment.java
+++ src/main/java/com/pizzeria/domain/commentdomain/Comment.java
@@ -1,3 +1,20 @@
+@Entity
 public class Comment {
+    @Entity
+    public class Comment {
+        @Id
+        @Type (type = "uuid-char")
+        public UUID id;
+    
+        @Column (nullable = false)
+        public String text;
+    
+        @Column (nullable = false)
+        public Date date;
+    
+        @Column (nullable = false)
+        public int rating;
+    }
     
 }
+
