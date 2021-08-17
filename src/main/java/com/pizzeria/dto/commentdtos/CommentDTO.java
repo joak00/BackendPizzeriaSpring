495fca078b42c902f1e1dbceb3c4e1a817809b77
Sergio Larrion <sergio.larrion-vidorreta@capgemini.com>
2021-08-17 18:52:03 +0200

    Creamos el DTO

---
+++ src/main/java/com/pizzeria/dto/commentdtos/CommentDTO.java
@@ -0,0 +1,11 @@
+package com.pizzeria.dto.commentdtos;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class CommentDTO {
+    public UUID id;
+    public String text;
+    public Date date;
+    public int score;
+}
---
+++ src/main/java/com/pizzeria/dto/commentdtos/CreateCommentDTO.java
@@ -0,0 +1,6 @@
+package com.pizzeria.dto.commentdtos;
+
+public class CreateCommentDTO {
+    public String text;
+    public int score;
+}
