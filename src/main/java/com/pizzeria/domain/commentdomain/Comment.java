9dc12f90fc823350d49407abb26cd16b40b166a0
Sergio Larrion <sergio.larrion-vidorreta@capgemini.com>
2021-08-17 18:40:20 +0200

    Arreglamos codigo y actualizamos el POM

--- src/main/java/com/pizzeria/domain/commentdomain/Comment.java
+++ src/main/java/com/pizzeria/domain/commentdomain/Comment.java
@@ -1,20 +1,26 @@
+package com.pizzeria.domain.commentdomain;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Type;
+
 @Entity
 public class Comment {
-    @Entity
-    public class Comment {
-        @Id
-        @Type (type = "uuid-char")
-        public UUID id;
-    
-        @Column (nullable = false)
-        public String text;
-    
-        @Column (nullable = false)
-        public Date date;
-    
-        @Column (nullable = false)
-        public int rating;
-    }
-    
-}
+    @Id
+    @Type(type = "uuid-char")
+    public UUID id;
 
+    @Column(nullable = false)
+    public String text;
+
+    @Column(nullable = false)
+    public Date date;
+
+    @Column(nullable = false)
+    public int rating;
+}
