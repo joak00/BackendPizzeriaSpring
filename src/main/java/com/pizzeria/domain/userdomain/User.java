34638238b45cd713f394de8761d6c67af9eb952a
Sergio Larrion <sergio.larrion-vidorreta@capgemini.com>
2021-08-25 11:13:27 +0200

    quitamos los imports innecesarios

--- src/main/java/com/pizzeria/domain/userdomain/User.java
+++ src/main/java/com/pizzeria/domain/userdomain/User.java
@@ -1,18 +1,13 @@
 package com.pizzeria.domain.userdomain;
 
-import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.pizzeria.core.EntityBase;
-
-import org.hibernate.annotations.Type;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
